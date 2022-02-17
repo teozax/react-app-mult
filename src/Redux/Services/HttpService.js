2a6a7a2c859dc3f7e9024f76da1aacea105ac2e1
unknown <theozacheilas44@gmail.com>
2022-02-18 01:50:12 +0200

    new commit

--- src/Redux/Services/HttpService.js
+++ src/Redux/Services/HttpService.js
@@ -3,7 +3,7 @@ export default class HttpService{
   SendRequest = async(url, RequestOptions) => {
     // const token = await localStorage.getItem(tokenId);
     // const requestOptions = this.RequestOptions(token, credentials);
-    return fetch(url, RequestOptions)
+    return fetch(url, {...RequestOptions,credentials: 'include',})
       .then(response=>{return response});
   }
 }
